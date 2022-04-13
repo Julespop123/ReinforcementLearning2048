575021c69e7c4ce4eded87a44a56f07f21b767fd
Julespop123 <86982596+Julespop123@users.noreply.github.com>
2022-04-12 18:05:08 -0700

    Formatting Changes

--- RL_2048.py
+++ RL_2048.py
@@ -17,26 +17,32 @@ import json
 # --------------Is it done?------------
 
 def test_done(A) :
+
     Done = True
     for i in range(1, 5) :
         for j in range(1,5) :
             if (A[i, j+1] == (0 or A[i, j])) or (A[i-1, j] == (0 or A[i, j])) or (A[i, j-1] == (0 or A[i, j])) or (A[i+1, j] == (0 or A[i, j])) :
                 Done = False
                 break
+
     return Done
 
 # ----------Random generation of 2's---------------
 
 def two_generator(A) :
+
     two = True
     rand_four = random.randint(1, 10)
+
     while two :
         new_x = random.randint(1, 4)
         new_y = random.randint(1, 4)
+
         if A[new_x, new_y] == 0 :
             if rand_four == 1 :
                 A[new_x, new_y] = 4
                 two = False
+
             else :
                 A[new_x, new_y] = 2
                 two = False
@@ -44,117 +50,145 @@ def two_generator(A) :
 # --------------Creation of the input Matrix (one hot encoded)----------------------
 
 def input_matrix_creation(A, Values) :
+
     input = 176
     square_num = 0
+
     for i in range(1, 5) :
         for j in range(1, 5) :
             for k in range(1, 12) :
                 if A[i, j] == 2**k :
                     Values[(square_num*11)+k-1] = 1
+
             square_num += 1
 
 # ----------------Creation of the input Matrix (normalization)-------------------
 
 def input_matrix_creation_norm(A, Values_2) :
+
     square_num = 0
+
     for i in range(1, 5) :
         for j in range(1, 5) :
             for k in range(1, 12) :
                 if A[i, j] == 2**k :
                     Values_2[square_num] = k / 11
+
             square_num += 1
 
 # ---------Shifting pieces over horizontally--------------
 
 def shift_horizontal(A, min_1, min_2, a) :
+
     i_v2 = min_1
     max_1 = min_1 + 4
     points = 0
     move = False
     merges_done = 0
+
     while i_v2 < max_1 :
         j_v2 = min_2
         max_2 = min_2 + 4
+
         while j_v2 < max_2 :
             i = abs(i_v2)
             j = abs(j_v2)
             b = 1
+
             while A[j, i+a*b] == 0 :
                 if A[j, i] != 0 :
                     move = True
                 A[j, i+a*b] = A[j, i+a*b-a]
                 A[j, i+a*b-a] = 0
                 b += 1
+
             if A[j, i+a*b] == A[j, i+a*b-a] :
                 merges_done += 1
                 A[j, i+a*b] = A[j, i+a*b] * 2
                 points =  points + A[j, i+a*b]
                 A[j, i+a*b-a] = 0
                 move = True
+
             j_v2 = j_v2 + 1
         i_v2 = i_v2 + 1
-    #    print ("Is there a move", move)
+
     if move == True :
         two_generator(A)
+
     return A, move, points, merges_done
 
 # ---------Shifting pieces over vertically--------------
 
 def shift_vertical(A, min_1, min_2, a) :
+
     i_v2 = min_1
     max_1 = min_1 + 4
     points = 0
     move = False
     merges_done = 0
+
     while i_v2 < max_1 :
         j_v2 = min_2
         max_2 = min_2 + 4
+
         while j_v2 < max_2 :
             i = abs(i_v2)
             j = abs(j_v2)
             b = 1
+
             while A[i+a*b, j] == 0 :
                 if A[i, j] != 0 :
                     move = True
                 A[i+a*b, j] = A[i+a*b-a, j]
                 A[i+a*b-a, j] = 0
                 b += 1
+
             if A[i+a*b, j] == A[i+a*b-a, j] :
                 merges_done += 1
                 A[i+a*b, j] = A[i+a*b, j] * 2
                 points = points + A[i+a*b, j]
                 A[i+a*b-a, j] = 0
                 move = True
+
             j_v2 = j_v2 + 1
         i_v2 = i_v2 + 1
-    #    print ("Is there a move", move)
+
     if move == True :
         two_generator(A)
+
     return A, move, points, merges_done
 
 # ------------Highest Number on the board----------------
 
 def highest_square(A) :
+
     high = 0
+
     for i in range(1, 5) :
         for j in range(1, 5) :
             if A[i, j] > high :
                 high = A[i, j]
+
     return high
 
 # ---------Movement on the board------------------
 
 def movement(A, direction) :
+
     print ("This is the direction", direction)
+
     if direction == 1 :  # Left
         A, move, points, merges_done = shift_horizontal(A, 1, 1, -1)
         Done = test_done(A)
+
     if direction == 2 :  # Right
         A, move, points, merges_done = shift_horizontal(A, -4, 1, 1)
         Done = test_done(A)
+
     if direction == 3 :  # Up
         A, move, points, merges_done = shift_vertical(A, 1, 1, -1)
         Done = test_done(A)
+
     if direction == 4 :  # Down
         A, move, points, merges_done = shift_vertical(A, -4, 1, 1)
         Done = test_done(A)
@@ -167,6 +201,7 @@ def movement(A, direction) :
 def display_game(A, total_points, gen, High_Score, decay_rate, Highest_number_seen) :
     
     screen.fill(WHITE)
+
     # Draw on the screen a line from (0,0) to (100,100)
     # 5 pixels wide.
     for i in range(1, 4) :
@@ -174,14 +209,16 @@ def display_game(A, total_points, gen, High_Score, decay_rate, Highest_number_se
         pygame.draw.line(screen, RED, [200*i, 0], [200*i, 800], 5)
     font = pygame.font.SysFont('Calibri', 75, True, False)
     font_2 = pygame.font.SysFont('Calibri', 25, True, False)
+
     for i in range(0, 4) :
         for j in range(0, 4) :
             text = font.render(str(int(A[i+1, j+1])), True, BLACK)
             screen.blit(text, [80+(200*j), 70+(200*i)])
+
     gen = font_2.render("Generation: " + str(int(gen)), True, BLACK)
     Present_score = font_2.render("Score: " + str(int(total_points)), True, BLACK)
     Score = font_2.render("High Score: " + str(int(High_Score)), True, BLACK)
-    Decay = font_2.render("Deacy Rate: " + str(float(decay_rate)), True, BLACK)
+    Decay = font_2.render("Decay Rate: " + str(round(decay_rate, 3)), True, BLACK)
     Big_Num = font_2.render("Biggest Number: " + str(float(Highest_number_seen)), True, BLACK)
     screen.blit(gen, [825, 25])
     screen.blit(Big_Num, [825, 150])
@@ -192,12 +229,15 @@ def display_game(A, total_points, gen, High_Score, decay_rate, Highest_number_se
 # --------------- Weighted Random Pick ------------------------
 
 def weighted_pick(array) :
+
     x = random.uniform(0, 1)
     cum_prob = 0.0
+
     for item in range(4) :
         cum_prob += array[item]
         if x < cum_prob :
             break
+
     return item
 
 
@@ -206,6 +246,7 @@ def weighted_pick(array) :
 ####################################
 
 # ---------------Initialize Pygame-------------------
+
 pygame.init()
 
 # ----------------Important Variables------------------
@@ -308,7 +349,6 @@ while not smart :
         Values = np.zeros(start_size)
         Old_A = np.zeros((6, 6))
         input_matrix_creation(A, Values) # Start Inputs
-#        input_matrix_creation_norm(A, Values)
 
         # --------------Choose which Epsilon to use------------------
         
@@ -333,6 +373,7 @@ while not smart :
         # is represented by 1 - 4. Else, use the model to predict the next move. 
         if np.random.rand() <= epsilon :
             action = np.random.randint(0, end_size, size = 1)
+            
         else :
             q = model.predict(Values.reshape(-1, start_size))
 
